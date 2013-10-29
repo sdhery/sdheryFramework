c7ab3eb7be3e25d33f560444c88985dc8f46cfab
sdhery@gmail.com <herys@163.com>
2013-10-29 17:33:38 +0800

    增加权限管理

---
+++ sdheryFramework-web/src/main/java/com/sdhery/module/privilege/domain/SysRole.java
@@ -0,0 +1,13 @@
+package com.sdhery.module.privilege.domain;
+
+import com.sdhery.module.core.domain.BaseEntity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sdhery
+ * Date: 13-10-29
+ * Time: 下午5:32
+ * 权限角色
+ */
+public class SysRole extends BaseEntity {
+}
