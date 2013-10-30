b7f482b64b4836a12ac8a433a0a681b82a00eafc
sdhery@gmail.com <herys@163.com>
2013-10-30 17:51:37 +0800

    修改权限

--- sdheryFramework-web/src/main/java/com/sdhery/module/privilege/dao/impl/SysRoleDao.java
+++ sdheryFramework-web/src/main/java/com/sdhery/module/privilege/dao/impl/SysRoleDao.java
@@ -3,6 +3,8 @@ package com.sdhery.module.privilege.dao.impl;
 import com.sdhery.module.core.dao.impl.BaseMybatisDao;
 import com.sdhery.module.privilege.dao.ISysRoleDao;
 import com.sdhery.module.privilege.domain.SysRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,5 +13,7 @@ import com.sdhery.module.privilege.domain.SysRole;
  * Time: 下午3:19
  * 权限角色
  */
+@Service
 public class SysRoleDao extends BaseMybatisDao<SysRole, Integer> implements ISysRoleDao {
+
 }
--- sdheryFramework-web/src/main/java/com/sdhery/module/privilege/service/impl/SysRoleService.java
+++ sdheryFramework-web/src/main/java/com/sdhery/module/privilege/service/impl/SysRoleService.java
@@ -1,8 +1,11 @@
 package com.sdhery.module.privilege.service.impl;
 
-import com.sdhery.module.core.dao.impl.BaseMybatisDao;
+import com.sdhery.module.core.dao.EntityDao;
+import com.sdhery.module.core.service.impl.BaseService;
 import com.sdhery.module.privilege.dao.ISysRoleDao;
 import com.sdhery.module.privilege.domain.SysRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,5 +14,12 @@ import com.sdhery.module.privilege.domain.SysRole;
  * Time: 下午3:39
  * 权限角色
  */
-public class SysRoleService extends BaseMybatisDao<SysRole, Integer> implements ISysRoleDao{
+@Service
+public class SysRoleService extends BaseService<SysRole, Integer> implements ISysRoleDao{
+    @Autowired
+    ISysRoleDao sysRoleDao;
+
+    protected EntityDao<SysRole, Integer> getEntityDao() {
+        return sysRoleDao;
+    }
 }
